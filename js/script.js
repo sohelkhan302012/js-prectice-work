2391368f450d9328b299b60aca76c998565d50cb
sohelkhan302012 <126311675+sohelkhan302012@users.noreply.github.com>
2024-09-20 22:26:30 +0530

    js prectice

---
+++ js/script.js
@@ -0,0 +1,347 @@
+// var firstStudentName = "sohel khan";
+// var firstStudentAge = 22;
+// var firstStudentAddress = "jhotwara jaipur";
+// var firstStudentMobileNo = 9079828177;
+// var firstStudentJobPost = "webdesigner";
+
+// console.log(
+//   firstStudentName,
+//   firstStudentAge,
+//   firstStudentMobileNo,
+//   firstStudentJobPost,
+//   firstStudentAddress
+// );
+
+// let $firstStudentName = "sohel khan";
+// let _firstStudentAge = 22;
+// let firstStudentAddress123 = "jhotwara jaipur";
+// let firstStudent_MobileNo = 9079828177;
+// let firstStude$ntJobPost = "webdesigner";
+
+// console.log(
+//   $firstStudentName,
+//   _firstStudentAge,
+//   firstStudent_MobileNo,
+//   firstStude$ntJobPost,
+//   firstStudentAddress123
+// );
+
+// const FIRST_STUDENT_NAME = "sohel khan";
+// const FIRST_STUDENT_AGE = 22;
+// const FIRST_STUDENT_ADDRESS = "jhotwara jaipur";
+// const FIRST_STUDENT_MOBILE_NO = 9079828177;
+// const FIRST_STUDENT_JOB_POST = "webdesigner";
+
+// console.log(
+//   FIRST_STUDENT_NAME,
+//   FIRST_STUDENT_AGE,
+//   FIRST_STUDENT_MOBILE_NO,
+//   FIRST_STUDENT_JOB_POST,
+//   FIRST_STUDENT_ADDRESS
+// );
+
+// var myHomeAddress = "Jhotwara Jipur";
+// var myVillageAddress = "Balghat";
+// var myCity = "Jaipur";
+// var myState = "Rajathan";
+// var myCountry = "India";
+
+// let myFirstSchool = "Saint meri school, jaipur";
+// let mySecondSchool = "little flower school ,sawai madhopur ";
+// let myThiredSchool = "government school, jaipur";
+// let myFourSchool = "government school, jaipur";
+// let myFiveSchool = "government school ,jaipur";
+
+// const MY_NAME = "SOHEL KHAN";
+// const MY_FATHER_NAME = "IMAMUDDIN";
+// const MY_MOTHER_NAME = "SHAHNAZ";
+// const MY_DOB = " 8 DECEMBER";
+// const MY_AADHAR_NO = "654493213451";
+// const MY_VOTER_NO = "RDR3785086";
+// const MY_PAN_NO = "KRZPK0647F";
+// const MY_8TH_SCORE = "A";
+// const MY_10TH_SCORE = "66%";
+// const MY_12TH_SCORE = "86.66%";
+// const MY_CITY_SECOND_NAME = "GULABI NAGRI";
+
+
+// console.log(typeof 123);
+// console.log(typeof "11");
+// console.log(typeof 14354.4565);
+// console.log(typeof "1@1");
+// console.log(typeof 0.34543545);
+
+// console.log(typeof "Sohel");
+// console.log(typeof "123213");
+// console.log(typeof "34$#ssjkck");
+// console.log(typeof "565.45645645");
+// console.log(typeof "$%$#%$#%$#%()$%$%{}");
+
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(2 == 2);
+// console.log(2 === 3);
+// console.log(2 === "2");
+// var i;
+// let age;
+// console.log(i, age);
+
+
+
+// console.log(20 + "5"); 
+// //  205
+// console.log("15" - 3);
+// // 12
+// console.log("8" + 2 + 3);
+// // 823
+// console.log("25" * "2");
+// // 50
+// console.log(true + "15");
+// // true15
+// console.log(false + 10);
+// // 10
+// console.log(10 + Number("10"));
+// // 20
+// console.log(String(10) + 5);
+// // 105
+// console.log("Hello" + 100);
+// // hello100
+// console.log("100" - true);
+// // 99
+// console.log(Number("50") + 30);
+// // 80
+// console.log(Boolean(0) + 5);
+// // 5
+// console.log("7" * true);
+// // 7true
+// console.log(100 - Number(null));
+// // 100
+// console.log(Number(false) + 25);
+// // 25
+// console.log(50 + " " + 25);
+// // 50 25
+// console.log(String(false) + 10);
+// // false10
+// console.log(Boolean("") + 10);
+// // 10
+// console.log("5" - "3" + true);
+// // 3
+// console.log("Hello" * 2);
+// // nan
+
+
+// console.log(true + "5" - false + 2 * "3");
+// // nan
+// console.log("10" * true + Number(false) + "20" - "5");
+// // 1015
+// console.log(5 + true * "10" / false + String(2 + 2));
+// // infinity14
+// console.log((Boolean("false") + 3) * (undefined + "5") - null);
+// // nan
+// console.log(Number("10") + Boolean("0") * ("15" / true) + "ABC" - false);
+// // nan
+
+
+// 2 ki table print
+//  * all multiples of 5 between 1 to 100
+//  * hello world print 50 times
+//  * 1-10 print
+//  * 10-1 print
+//  * 2 ki table ulti print krvadi 20 18
+
+// 2 table
+
+// for(let a = 1; a <= 10; ++a){
+//     console.log(a*2)
+// }
+
+//  * all multiples of 5 between 1 to 100
+
+// for(let b = 1; b <= 100; ++b){
+//     if(b % 5 == 0){
+//         console.log(b)
+//     }
+// }
+
+//  * hello world print 50 times
+
+// for( let c = 1; c <= 50 ; ++c){
+//     console.log("helloWorld")
+// } 
+
+//  * 1-10 print
+//  for( let d = 1; d <= 10; ++d){
+//     console.log(d)
+//  }
+
+//  * 10-1 print
+// for( let e = 10; e >= 1; --e){
+//     console.log(e)
+//  }
+
+//  * 2 ki table ulti print krvadi 20 18
+
+// for( let f = 10 ; f >= 1; --f){
+//     console.log(f*2)
+// }
+
+//print 1 - 100
+
+// for(let g = 1; g <= 100; ++g){
+//  console.log(g);
+// }
+
+//Print all Odd numbers from 1 to 100.
+
+// for(let h  = 1; h <= 100; ++h){
+//     if(h % 2 == 1){
+//         console.log(h);
+        
+//     }
+// }
+//Print all Even numbers from 1 to 100.
+
+// for(let i = 1; i <= 100; ++i){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+//Print all numbers which are divisible of 5 and 15 in 1 to 100.
+
+// for( let j = 1; j <= 100; ++j){
+//     if(j % 5 == 0 && j % 15 == 0){
+//         console.log(j);
+//     }
+// }
+
+//Calculate sum of all numbers from 1 to 100.
+//  let l = 0;
+// for( let k = 1 ; k <= 100; ++k){
+//  l = l + k;
+// }
+// console.log(l); 
+
+//Calculate sum of odd all numbers from 1 to 100.
+//  let m = 0;
+// for( let n = 1 ; n <= 100; ++n){
+// if( n % 2 == 1){
+//      m = m + n;
+// }
+// }
+// console.log(m); 
+//Calculate sum of odd all numbers from 1 to 100.
+//  let m = 0;
+// for( let n = 1 ; n <= 100; ++n){
+// if( n % 2 == 0){
+//      m = m + n;
+// }
+// }
+// console.log(m); 
+
+// Print 1,2,4,8,16,32,64.......upto 2048.
+// for( let o = 1 ; o <= 2068;  o = o * 2){
+// console.log(o); 
+// }
+
+
+ 
+
+// 7. Print table of 100
+
+
+// Find sum of all numbers from 1 to 20 
+// let total = 0;
+// for (let sum = 1; sum <= 20; ++sum){
+//     total = total + sum;
+// }
+// console.log(total);
+
+
+
+// 2. Find count of all odd numbers from 1 to 20
+// let oddCount = 0;
+// for (let odd = 1; odd <= 20;  ++odd){
+//     if( odd % 2 !== 0){
+// ++oddCount;
+//     }   
+// }
+// console.log(oddCount);
+
+
+// 3. Find count of all even numbers from 1 to 20 
+// let evenCount = 0;
+// for (let even = 1; even <= 20;  ++even){
+//     if( even % 2 == 0){
+// ++evenCount;
+//     }   
+// }
+// console.log(evenCount);
+
+
+// 4. Find count of all numbers which are divisible by 4 and 8 between from 1 to 100
+// let divisibleCount = 0;
+// for(let count48 = 1; count48 <= 100; ++count48){
+
+//     if(count48 % 4 === 0 && count48 % 8 === 0){
+// ++divisibleCount;
+//     }
+// }
+// console.log(divisibleCount)
+
+// 5. Print all odd numbers from 50 to 1 in reverse order 
+
+// for(let reverseOrder = 50; reverseOrder >= 1; --reverseOrder){
+//     if(reverseOrder % 2 == 1){
+//         console.log(reverseOrder);
+//     }
+// }
+
+// 6. Ask user "How many times do you want to print?" and then ask "What do you want to print" and then print it.
+//  let answer1 = prompt("How many times do you want to print?");
+//  let answer2 = prompt("What do you want to print?");
+//   for(let i = +1; i <= answer1; ++i){
+//     console.log(answer2);
+//   }
+
+// 7. Print table of 100
+
+// for(let j = 1; j <= 10; ++j){
+//     console.log(j*100);
+    
+// }
+
+// for( let z = 1; z <= 100; ++z){
+// console.log(z);
+// if( z % 10 == 0){
+//     console.log("<br>");
+// }
+
+// }
+
+// for( let y = 1; y <= 100; ++y){
+
+    
+//     if( y % 3 == 0 && y % 5 == 0){
+//         console.log("wecode academy");
+//     }
+//     else if ( y % 3 == 0){
+//         console.log("academy");
+//     }
+//     else if( y % 5 == 0 ){
+//         console.log("wecode ");
+//     }
+//     else{
+//         console.log(y)
+//     }
+
+// }
+
+
+let table = +prompt("what is number for table");
+
+for ( let i = 1; i <= 10; i++){
+    console.log( `${table} x ${i} = ${ i * table }`);
+    
+}
+ 
